7e9b48d52549599810f062983de9376fdc2667d9
enilu <eniluzt@qq.com>
2019-08-21 15:16:40 +0800

    add log

--- material-manage/src/main/java/cn/enilu/material/admin/interceptor/WebInterceptor.java
+++ material-manage/src/main/java/cn/enilu/material/admin/interceptor/WebInterceptor.java
@@ -4,6 +4,8 @@ import cn.enilu.material.bean.enumeration.ConfigKeyEnum;
 import cn.enilu.material.bean.vo.SpringContextHolder;
 import cn.enilu.material.dao.cache.ConfigCache;
 import cn.enilu.material.dao.cache.impl.ConfigCacheImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -20,6 +22,7 @@ import javax.servlet.http.HttpServletResponse;
 public class WebInterceptor implements HandlerInterceptor {
     @Autowired
     private ConfigCache configCache;
+    private Logger logger = LoggerFactory.getLogger(WebInterceptor.class);
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
 
